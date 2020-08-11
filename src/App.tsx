1ede963787306fd430d9fa030b0a2278d3961f11
Ravindra Kauthale <Ravindra.Kauthale@extentia.com>
2020-08-11 11:27:00 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
+  const [count, setState] = useState(0);
+  const [count1, setStat1e] = useState(0);
+  const increment=()=>{
+    setState(pre => pre+1)
+  }
+
+  useEffect(()=>{
+    console.log('this is invoked')
+    document.title = `You clicked ${count} times`;
+  },[count])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={increment}>Increment</button>
+        <button onClick={()=>setStat1e(prev=>prev+1)}>Increment {count1}</button>
+        <span>This is the count value {count}</span>
     </div>
   );
 }
